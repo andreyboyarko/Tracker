41a8c83c7736025940783d9fa1e4c0acbc325d92
Andrei Boyarko <andreyboyarko@gmail.com>
2025-08-29 12:59:24 +0200

    Init: Tracker UIKit project

---
+++ Tracker/TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Andrei  Boyarko on 29/08/2025.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
